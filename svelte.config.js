c34d601fc09dbbe90771ebb862cd434b131ed071
Kolja <lasar@rasal.de>
2024-12-29 23:49:45 +0100

    refactor: remove unused files and update configuration for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ pages: 'docs' })
+		adapter: adapter()
+		// adapter: adapter({ pages: 'docs' })
 	},
 	paths: {
 		base: process.env.NODE_ENV === 'production' ? '/Svelte-ReaPer-Store' : ''
